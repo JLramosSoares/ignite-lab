b34bf168c660b73573fa883eabaeb2860dd3fc1d
JLramosSoares <83511122+JLramosSoares@users.noreply.github.com>
2022-06-24 15:51:43 -0300

    Criação do Sidebar e vínculo com API das Lesson

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,10 +1,10 @@
-import { Header } from './components/Header'
+import { Event } from "./pages/Event"
 
 function App() {
 
   return (
     <div>
-      <Header />
+      <Event />
     </div>
   )
 }
